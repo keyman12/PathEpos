344331feb1de125a010a59df6f2ea071628d4e9c
David Key <Davidkey75@gmail.com>
2025-08-29 08:55:10 +0100

    Initial Commit

---
+++ PathEPOSDemo/PathEPOSDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathEPOSDemoApp.swift
+//  PathEPOSDemo
+//
+//  Created by David Key on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PathEPOSDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PathEPOSDemoTests/PathEPOSDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PathEPOSDemoTests.swift
+//  PathEPOSDemoTests
+//
+//  Created by David Key on 29/08/2025.
+//
+
+import Testing
+@testable import PathEPOSDemo
+
+struct PathEPOSDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
